99e1ace01fddbae80f13d3c1f547dc113065807a
Sieger06 <irek.haibulin@gmail.com>
2023-03-29 11:41:44 +0300

    Initial commit

---
+++ src/main/java/com/example/intromaven2/IntroMaven2Application.java
@@ -0,0 +1,13 @@
+package com.example.intromaven2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IntroMaven2Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(IntroMaven2Application.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/intromaven2/IntroMaven2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.intromaven2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IntroMaven2ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
